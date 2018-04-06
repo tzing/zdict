469eb54e3651e253704bbbb3d1d294a28f62e960
Tzu-ting <tzingshih@gmail.com>
2018-04-06 22:30:12 +0800

    change cache path

--- zdict/constants.py
+++ zdict/constants.py
@@ -3,8 +3,8 @@ import os
 
 VERSION = '0.11.0'
 
-BASE_DIR_NAME = '.zdict'
-BASE_DIR = os.path.join(os.path.expanduser("~"), BASE_DIR_NAME)
+BASE_DIR_NAME = 'zdict'
+BASE_DIR = os.path.join(os.path.expanduser('~/.cache'), BASE_DIR_NAME)
 
 DB_NAME = 'zdict.db'
 DB_FILE = os.path.join(BASE_DIR, DB_NAME)
